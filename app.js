d4cd7829b48d2486884014c64acd5d196fd3da0a
Jory Schmidt <joryschmidt1988@gmail.com>
2016-08-01 22:48:31 +0000

    Added some new directives

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-var app = angular.module('store', []);
+(function() {
+  var app = angular.module('store', []);
+  
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+  
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 1200,
+      description: 'Shiny. Bold. Alien. A must have item.',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Ruby', 
+      price: 3000,
+      description: "Red and plump",
+      canPurchase: true,
+      soldOut: true
+    }
+  ];
+})();
