0357690eaca7843a048825aa4f1e2757a8a1c669
Jory Schmidt <joryschmidt1988@gmail.com>
2016-08-08 18:43:11 +0000

    refactor code into element directives

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 (function() {
-  var app = angular.module('store', []);
+  var app = angular.module('store', ['store-products']);
   
   app.controller('PanelController', function() {
-    this.tab = 1;
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab) {
-      return this.tab == checkTab;
-    };
   });
   
   app.controller('StoreController', function() {
