b4adbaf589c75384d2215386058a0f7012f4b535
Jory Schmidt <joryschmidt1988@gmail.com>
2016-08-08 20:05:07 +0000

    refactor into directives and add http service

--- app.js
+++ app.js
@@ -4,9 +4,14 @@
   app.controller('PanelController', function() {
   });
   
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+  app.controller('StoreController', ['$http', function($http) {
+    var store = this;
+    store.products = [];
+    
+    $http.get('products.json').success(function(data) {
+      store.products = data;
+    });
+  }]);
   
   app.controller('ReviewController', function() {
     this.review = {};
@@ -20,28 +25,4 @@
       return count == 0;
     };
   });
-  
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 1200,
-      description: 'Shiny. Bold. Alien. A must have item.',
-      canPurchase: true,
-      soldOut: false,
-      reviews: [
-        {
-          stars: 3,
-          body: "I really liked this one",
-          author: "JoJo@yahoo.com"
-        } 
-      ]
-    },
-    {
-      name: 'Ruby', 
-      price: 3000,
-      description: "Red and plump",
-      canPurchase: true,
-      soldOut: true
-    }
-  ];
 })();
